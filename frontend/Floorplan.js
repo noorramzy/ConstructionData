ee918096213584fd0e112b2308c2f57aa2989ce6
William Edwards <wedwards23@gds.org>
2023-03-20 14:44:43 -0400

    make a floorplan

---
+++ frontend/Floorplan.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Box() {
+    return (
+        <Box
+        style={{
+            backgroundColor: '#333',
+            borderRadius: 4,
+            color: '#eee',
+            minHeight: 200,
+            padding: 12,
+            width: 300,
+        }}
+        >
+        john
+        </Box>
+    )
+    }
