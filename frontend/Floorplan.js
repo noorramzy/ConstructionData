dd88740df5d1a5b5dabee4d7ce3b8cb6d65ad42f
William Edwards <wedwards23@gds.org>
2023-04-05 14:59:29 -0400

    floorplan is made although extremely mediocre

--- frontend/Floorplan.js
+++ frontend/Floorplan.js
@@ -4,15 +4,99 @@ export default function Box() {
     return (
         <div
         style={{
-            backgroundColor: '#333',
+            backgroundColor: "black",
             borderRadius: 4,
             color: '#eee',
-            minHeight: 200,
+            minHeight: 350,
             padding: 12,
-            width: 300,
+            width:500,
+            position: "absolute",
+            top: 150,
+            left: 0,
         }}
         >
-        john
+            <div
+                style={{
+                backgroundColor: "red",
+                borderRadius: 4,
+                color: '#eee',
+                minHeight: 80,
+                padding: 12,
+                width: 175,
+                position: "absolute",
+                top: 0,
+                left: 0,
+            
+            }}
+            >
+            counter
+            </div>
+            <div
+                style={{
+                backgroundColor: "blue",
+                borderRadius: 4,
+                color: '#eee',
+                minHeight: 80,
+                padding: 12,
+                width: 80,
+                position: "absolute",
+                top: 0,
+                left: 175,
+            
+            }}
+            >
+            sink
+            </div>
+            <div
+                style={{
+                backgroundColor: "pink",
+                borderRadius: 4,
+                color: '#eee',
+                minHeight: 80,
+                padding: 12,
+                width: 125,
+                position: "absolute",
+                top: 0,
+                left: 255,
+            
+            }}
+            >
+            stove
+            </div>
+            <div
+                style={{
+                backgroundColor: "silver",
+                borderRadius: 4,
+                color: '#eee',
+                minHeight: 150,
+                padding: 12,
+                width: 80,
+                position: "absolute",
+                top: 80,
+                left: 0,
+            
+            }}
+            >
+            fridge
+            </div>
+            <div
+                style={{
+                backgroundColor: "yellow",
+                borderRadius: 4,
+                color: '#eee',
+                minHeight: 100,
+                padding: 12,
+                width: 275,
+                position: "absolute",
+                top: 200,
+                left: 150,
+            
+            }}
+            >
+            island
+            </div>
+            floor
         </div>
     )
     }
+
--- frontend/index.js
+++ frontend/index.js
@@ -1,34 +1,22 @@
-import { initializeBlock } from '@airtable/blocks/ui';
-import React, { useState } from 'react';
-import ConstructionList from './ConstructionList';
+import {initializeBlock} from '@airtable/blocks/ui';
+import React, {useState} from 'react';
+import ConstructionList from './ConstructionList'
 import Box from './Floorplan';
 
 function ConstructionTest() {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [location, setLocation] = useState('DC');
+    const [searchTerm, setSearchTerm] = useState()
 
     function updateSearchTerm(event) {
-        setSearchTerm(event.target.value);
+        setSearchTerm(event.target.value)
     }
 
-    function handleLocChange(event) {
-        setLocation(event.target.value);
-    }
+    return <div>
+        <input onChange={updateSearchTerm} />
+        <ConstructionList searchTerm={searchTerm} />
+        <Box/>
+        
 
-    return (
-        <div>
-            <div>
-                <select value={location} onChange={handleLocChange}>
-                    <option value="DC">DC</option>
-                    <option value="Maryland">MD</option>
-                    <option value="Virginia">VA</option>
-                </select>
-            </div>
-            <input id="term" value={searchTerm} onChange={updateSearchTerm} />
-            <ConstructionList searchTerm={searchTerm} location={location}/>
-            <Box />
-        </div>
-    );
+    </div>;
 }
 
 initializeBlock(() => <ConstructionTest />);
