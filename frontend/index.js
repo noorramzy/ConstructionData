9f6cb30317eb8ef4e8d0354dff3c4b0082430919
JYuan23 <jyuan23@gds.org>
2023-04-06 11:06:36 -0400

    fix index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,22 +1,34 @@
-import {initializeBlock} from '@airtable/blocks/ui';
-import React, {useState} from 'react';
-import ConstructionList from './ConstructionList'
+import { initializeBlock } from '@airtable/blocks/ui';
+import React, { useState } from 'react';
+import ConstructionList from './ConstructionList';
 import Box from './Floorplan';
 
 function ConstructionTest() {
-    const [searchTerm, setSearchTerm] = useState()
+    const [searchTerm, setSearchTerm] = useState('');
+    const [location, setLocation] = useState('DC');
 
     function updateSearchTerm(event) {
-        setSearchTerm(event.target.value)
+        setSearchTerm(event.target.value);
     }
 
-    return <div>
-        <input onChange={updateSearchTerm} />
-        <ConstructionList searchTerm={searchTerm} />
-        <Box/>
-        
+    function handleLocChange(event) {
+        setLocation(event.target.value);
+    }
 
-    </div>;
+    return (
+        <div>
+            <div>
+                <select value={location} onChange={handleLocChange}>
+                    <option value="DC">DC</option>
+                    <option value="Maryland">MD</option>
+                    <option value="Virginia">VA</option>
+                </select>
+            </div>
+            <input id="term" value={searchTerm} onChange={updateSearchTerm} />
+            <ConstructionList searchTerm={searchTerm} location={location}/>
+            <Box />
+        </div>
+    );
 }
 
 initializeBlock(() => <ConstructionTest />);
