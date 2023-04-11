acb1e6b9bdae14f305ba60b9455f280f30707c44
JYuan23 <jyuan23@gds.org>
2023-04-11 14:32:55 -0400

    moved ConstructionTest to Homepage

---
+++ frontend/Homepage.js
@@ -0,0 +1,49 @@
+import { initializeBlock } from '@airtable/blocks/ui';
+import React, { useState } from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import ConstructionList from './ConstructionList';
+import Box from './Floorplan';
+import "./index.css";
+
+export default function Homepage() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [location, setLocation] = useState('DC');
+
+  const handleSearchTermChange = ({ target: { value } }) => {
+    setSearchTerm(value);
+  }
+
+  const handleLocationChange = ({ target: { value } }) => {
+    setLocation(value);
+  }
+
+  return (
+    <div>
+      {/* <div className="bar">
+        <div>
+          <NavLink>Test 1</NavLink>
+          <NavLink>Test 2</NavLink>
+          <NavLink>Test 3</NavLink>
+        </div>
+        <Routes>
+          <Route path="/" element={<ConstructionTest></ConstructionTest>}></Route>
+        </Routes>
+      </div>  */}
+
+      <div className="dropdown">
+        <label htmlFor="location">Location:</label>
+        <select id="location" value={location} onChange={handleLocationChange}>
+          <option value="DC">DC</option>
+          <option value="Maryland">MD</option>
+          <option value="Virginia">VA</option>
+        </select>
+      </div>
+      <div id="searchBar">
+        <label htmlFor="search">Search Item:</label>
+        <input id="search" value={searchTerm} onChange={handleSearchTermChange} />
+      </div>
+      <ConstructionList searchTerm={searchTerm} location={location} />
+      <Box />
+    </div>
+  );
+}
--- frontend/index.js
+++ frontend/index.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Routes, Route, NavLink } from 'react-router-dom';
 import ConstructionList from './ConstructionList';
 import Box from './Floorplan';
+import Homepage from './Homepage';
 import "./index.css";
 
 const ConstructionTest = () => {
@@ -19,30 +20,7 @@ const ConstructionTest = () => {
 
   return (
     <div>
-      {/* <div className="bar">
-        <div>
-          <NavLink>Test 1</NavLink>
-          <NavLink>Test 2</NavLink>
-          <NavLink>Test 3</NavLink>
-        </div>
-        <Routes>
-          <Route path="/" element={}></Route>
-        </Routes>
-      </div> */}
-
-      <div className="dropdown">
-        <label htmlFor="location">Location:</label>
-        <select id="location" value={location} onChange={handleLocationChange}>
-          <option value="DC">DC</option>
-          <option value="Maryland">MD</option>
-          <option value="Virginia">VA</option>
-        </select>
-      </div>
-      <div id="searchBar">
-        <label htmlFor="search">Search Item:</label>
-        <input id="search" value={searchTerm} onChange={handleSearchTermChange} />
-      </div>
-      <ConstructionList searchTerm={searchTerm} location={location} />
+      <Homepage />
       <Box />
     </div>
   );
