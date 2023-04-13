1ccdf594b86aebed49c388b2e226c2caec418aae
JYuan23 <jyuan23@gds.org>
2023-04-13 10:50:00 -0400

    floorplan edits

--- frontend/Floorplan.js
+++ frontend/Floorplan.js
@@ -1,104 +1,67 @@
-import React from "react"
-import Draggable from "react-draggable"
+import React, { useState } from "react";
+import Draggable from "react-draggable";
+
+export const Item = (props) => {
+  const [grid, setGrid] = useState([10, 10]);
+
+  return (
+    <Draggable grid={grid}>
+      <div
+        style={{
+          backgroundColor: props.color,
+          borderRadius: 4,
+          color: "#eee",
+          minHeight: parseInt(props.height),
+          padding: 12,
+          width: parseInt(props.width),
+          position: "absolute",
+          top: 0,
+          left: 0,
+        }}
+      >
+        {props.itemName}
+      </div>
+    </Draggable>
+  );
+};
 
 export default function Box() {
-    return (
-        
-        <div
-        style={{
-            backgroundColor: "black",
-            borderRadius: 4,
-            color: '#eee',
-            minHeight: 350,
-            padding: 12,
-            width:500,
-            position: "absolute",
-            top: 800,
-            left: 0,
-        }}
-        >
-            <Draggable><div
-                style={{
-                backgroundColor: "red",
-                borderRadius: 4,
-                color: '#eee',
-                minHeight: 80,
-                padding: 12,
-                width: 175,
-                position: "absolute",
-                top: 0,
-                left: 0,
-            
-            }}
-            >
-            counter
-            </div></Draggable>  
-            <Draggable><div
-                style={{
-                backgroundColor: "blue",
-                borderRadius: 4,
-                color: '#eee',
-                minHeight: 80,
-                padding: 12,
-                width: 80,
-                position: "absolute",
-                top: 0,
-                left: 175,
-            
-            }}
-            >
-            sink
-            </div></Draggable>
-            <Draggable><div
-                style={{
-                backgroundColor: "pink",
-                borderRadius: 4,
-                color: '#eee',
-                minHeight: 80,
-                padding: 12,
-                width: 125,
-                position: "absolute",
-                top: 0,
-                left: 255,
-            
-            }}
-            >
-            stove
-            </div></Draggable>
-            <Draggable><div
-                style={{
-                backgroundColor: "silver",
-                borderRadius: 4,
-                color: '#eee',
-                minHeight: 150,
-                padding: 12,
-                width: 80,
-                position: "absolute",
-                top: 80,
-                left: 0,
-            
-            }}
-            >
-            fridge
-            </div></Draggable>
-            <Draggable><div
-                style={{
-                backgroundColor: "yellow",
-                borderRadius: 4,
-                color: '#eee',
-                minHeight: 100,
-                padding: 12,
-                width: 275,
-                position: "absolute",
-                top: 200,
-                left: 150,
-            
-            }}
-            >
-            island
-            </div></Draggable>
-            floor
-        </div>
-    )
-    }
+  const [showGrid, setShowGrid] = useState(false);
 
+  return (
+    <div
+      style={{
+        position: "relative",
+      }}
+    >
+      <div
+        className="outerBox"
+        style={{
+          backgroundColor: "black",
+          borderRadius: 4,
+          color: "#eee",
+          minHeight: 350,
+          padding: 12,
+          width: 500,
+          position: "absolute",
+          top: 50,
+          left: 0,
+        }}
+      >
+        <Item color="red" height="50" width="90" itemName="test" />
+        <Item color="blue" height="80" width="80" itemName="sink" />
+        <Item color="pink" height="80" width="125" itemName="Stove" />
+        <Item color="silver" height="150" width="80" itemName="Fridge" />
+        <Item color="yellow" height="100" width="275" itemName="Island" />
+
+        floor
+      </div>
+
+      <div style={{ position: "absolute", top: 10, left: 10 }}>
+        <button onClick={() => setShowGrid(!showGrid)}>
+          Toggle snap to grid
+        </button>
+      </div>
+    </div>
+  );
+}
