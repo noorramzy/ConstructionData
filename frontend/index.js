eaf383384acf8fbd60cc46939fb18ede28c9dfcd
JYuan23 <jyuan23@gds.org>
2023-04-21 13:12:42 -0400

    navigation bar

--- frontend/index.js
+++ frontend/index.js
@@ -1,27 +1,26 @@
+// ConstructionTest.js
+
 import { initializeBlock } from '@airtable/blocks/ui';
 import React, { useState } from 'react';
 import { Routes, Route, NavLink, Router } from 'react-router-dom';
 import ConstructionList from './ConstructionList';
 import Box from './Floorplan';
-import {HashRouter} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Homepage from './Homepage';
-import "./index.css";
+import './ConstructionTest.css';
 
 const ConstructionTest = () => {
   return (
-      
-      <HashRouter>
-        
-        {/* <Homepage /> */}
-
-        <NavLink to="/"> Home</NavLink>
-        <NavLink to="/floorplan">Floorplan</NavLink>
-
-        <Routes>
-          <Route path="" element={<Homepage></Homepage>}></Route>
-          <Route path="/floorplan" element={<Box></Box>}></Route>
-        </Routes>
-      </HashRouter>
+    <HashRouter>
+      <nav className="navbar">
+        <NavLink to="/" className="navlink" activeClassName="active">Home</NavLink>
+        <NavLink to="/floorplan" className="navlink" activeClassName="active">Floorplan</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/floorplan" element={<Box />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
