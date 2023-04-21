f147a4698f0fcc374357f0fec2cbd39d7fa193ec
JYuan23 <jyuan23@gds.org>
2023-04-20 22:16:07 -0400

    add items to list and price

--- frontend/ConstructionList.js
+++ frontend/ConstructionList.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import './ConstructionList.css';
 
 export default function ConstructionList(props) {
+  const [nextItemId, setNextItemId] = useState(1); 
+  const [selectedItems, setSelectedItems] = useState([]);
   const searchTerm = props.searchTerm;
   const base = useBase();
   const roomType = props.roomType;
@@ -21,6 +23,30 @@ export default function ConstructionList(props) {
       .toLowerCase()
       .includes(searchTerm.toLowerCase())
   );
+
+  const handleAddItem = (record) => {
+    const newItem = {
+      id: nextItemId, // <-- use current ID number
+      record: record,
+      materials: record.getCellValueAsString('Materials'),
+      price: record.getCellValue('Price')
+    };
+    setSelectedItems([...selectedItems, newItem]);
+    setNextItemId(nextItemId + 1); // <-- increment ID number
+  };
+
+  const handleRemoveItem = (itemId) => {
+    setSelectedItems(selectedItems.filter((item) => item.id !== itemId));
+  };
+  
+  const handleClearItems = () => {
+    setSelectedItems([]);
+  };
+
+  const totalPrice = selectedItems.reduce((acc, item) => {
+    return acc + parseFloat(item.price) * priceCoeff;
+  }, 0);
+
   return (
     <div className="construction-list-container">
       <h2 className="construction-list-header">Construction Materials</h2>
@@ -32,12 +58,36 @@ export default function ConstructionList(props) {
             </span> 
             <span className="construction-list-item-price">
               ${parseFloat((record.getCellValueAsString('Price'))*priceCoeff).toFixed(2).toLocaleString()}
-              <button className="selectButton">test</button>
+              <button className="selectButton" onClick={() => handleAddItem(record)}>
+                Add
+              </button>
             </span>
-
           </li>
         ))}
       </ul>
+      <div className="selected-items-container">
+        <h3 className="selected-items-header">Selected Items:</h3>
+        <button className="clearButton" onClick={handleClearItems}>
+          Clear All
+        </button>
+        <ul className="selected-items-list">
+          {selectedItems.map((item) => (
+            <li key={item.id} className="selected-items-list-item">
+              <span>{item.record.getCellValueAsString('Materials')}</span>
+              <span>
+                ${parseFloat(item.record.getCellValue('Price') * priceCoeff).toFixed(2).toLocaleString()}
+                <button className="removeButton" onClick={() => handleRemoveItem(item.id)}>
+                  Remove
+                </button>
+              </span>
+            </li>
+          ))}
+        </ul>
+        <div className="total-price-container">
+          <span className="total-price-label">Total Price:</span>
+          <span className="total-price-value">${totalPrice.toFixed(2)}</span>
+        </div>
+      </div>
     </div>
   );
 }
